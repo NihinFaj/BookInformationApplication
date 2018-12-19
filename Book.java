0ff673b83533d1af0aa5ebec7cf3f228bb36c782
Nihinlolamiwa Fajemilehin <nihinfaj@gmail.com>
2018-12-19 14:29:52 +0000

    Initial sharing of project

---
+++ Book.java
@@ -0,0 +1,105 @@
+/**
+ * A class that maintains information on a book.
+ * This might form part of a larger application such
+ * as a library system, for instance.
+ *
+ * @author (Nihinlolamiwa Donaldson Fajemilehin)
+ * @version (4th of October, 2018)
+ */
+public class Book
+{
+    // The fields.
+    private String author;
+    private String title;
+    private int pages;
+    private String refNumber;
+    private int borrowed;
+
+    /**
+     * Set the author and title fields when this object
+     * is constructed.
+     */
+    public Book(String bookAuthor, String bookTitle, int bookPages)
+    {
+        author = bookAuthor;
+        title = bookTitle;
+        pages = bookPages;
+        refNumber = "";
+    }
+
+    //Method to print book Author name
+    public void printAuthor()
+    {
+        System.out.println("Author name is " + author);
+    }
+
+    //Method to print book Title
+    public void printTitle()
+    {
+        System.out.println("Book title is " + title);
+    }
+
+    //Method to get book author name
+    public String getAuthor()
+    {
+        return author;
+    }
+
+    //Method to get book title
+    public String getTitle()
+    {
+        return title;
+    }
+
+    //Method to get book pages
+    public int getPages()
+    {
+        return pages;
+    }
+
+    //Method to print the details of the book
+    public void printDetails()
+    {
+        System.out.println("Book author name is: " + author);
+        System.out.println("Book title is: " + title);
+        System.out.println("Number of book pages is: " + pages);
+        if(refNumber.length() > 0) {
+            System.out.println("Book reference number is: " + refNumber);
+        }
+        else { 
+            System.out.println("Book reference number is: ZZZ");
+        }
+        System.out.println("The number of times book has been borrowed is: " + borrowed);
+        System.out.println();
+    }
+    
+    //Method to set book reference number
+    public void setRefNumber(String ref)
+    {
+        if(ref.length() >= 3) {
+            refNumber = ref;
+        }
+        else {
+            System.out.println("Reference Number must be at least 3 characters");
+        }
+    }
+    
+    //Method to get book reference Number
+    public String getRefNumber()
+    {
+        return refNumber;
+    }
+    
+    //Method to count how many times a book has been borrowed
+    public void borrow()
+    {
+        borrowed = borrowed + 1;
+    }
+    
+    //Method to get the number of times a book has been borrowed
+    public int getBorrowed()
+    {
+        return borrowed;
+    }
+    
+}
